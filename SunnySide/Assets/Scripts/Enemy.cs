96c5e4ec3aa8ede2b1afe7596640cc67b84dc986
rlagusrn08 <rlagusrn08@gmail.com>
2020-11-11 18:13:01 +0900

    프로젝트 시작!

---
+++ SunnySide/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    protected AudioSource death;
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        death = GetComponent<AudioSource>();
+    }
+
+    public void JumpedOn()
+    {
+        anim.SetTrigger("Death");
+        death.Play();
+        rb.velocity = Vector2.zero;
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ SunnySide/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+
+
+    [SerializeField] private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int cherries = 0;
+    [SerializeField] private Text cherryText;
+    [SerializeField] private float hurtForce = 10f;
+    [SerializeField] private AudioSource cherry;
+    [SerializeField] private AudioSource footstep;
+
+
+    private enum State { idle, running, jumping, falling, hurt }
+    private State state = State.idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state != State.hurt)
+        {
+            Movement();
+        }
+        VelocityState();
+        anim.SetInteger("State", (int)state);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectable")
+        {
+            Destroy(collision.gameObject);
+            cherry.Play();
+            cherries += 1;
+            cherryText.text = cherries.ToString();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            if(state == State.falling)
+            {
+                enemy.JumpedOn();
+                Jump();
+            }
+            else
+            {
+                state = State.hurt;
+                if(collision.gameObject.transform.position.x > transform.position.x)
+                {
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+        }
+    }
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+
+        }
+
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+
+        }
+
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        state = State.jumping;
+    }
+
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+            if (rb.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if(state == State.hurt)
+        {
+            if(Mathf.Abs(rb.velocity.x) < 0.1f)
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+    private void Footstep()
+    {
+        footstep.Play();
+    }
+}
