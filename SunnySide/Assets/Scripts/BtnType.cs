209a7ad3f62eaf3c8ec3c0fa667fea96a4396304
gliese71004 <gliese710@naver.com>
2020-11-22 21:28:01 +0900

    배경추가,타이틀 추가
    메인메뉴 만들고 자잘하게 수정함!

---
+++ SunnySide/Assets/Scripts/BtnType.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BtnType : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource button_press;
+    public GameObject How_To_pop;
+    public GameObject btn_menu;
+    public BTNType currentType;
+
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+
+            case BTNType.Start:
+
+                Debug.Log("시작");
+                SceneManager.LoadScene("SampleScene");
+                button_press.Play();
+
+                break;
+
+            case BTNType.How_To:
+
+                How_To_pop.SetActive(true);
+                btn_menu.SetActive(false);
+                button_press.Play();
+
+
+                break;
+
+            case BTNType.Exit:
+                
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit()
+            #endif
+
+                break;
+
+            case BTNType.Quit:
+               
+
+                  How_To_pop.SetActive(false);
+                  btn_menu.SetActive(true);
+               
+                button_press.Play();
+
+                break;
+        }
+    }
+}
+
---
+++ SunnySide/Assets/Scripts/MainUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BTNType
+{
+    Start,
+    How_To,
+    Exit,
+    Quit
+}
+
+public class MainUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
